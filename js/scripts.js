877759f748ebf7c3b8959f00fa6432c7bbe326f0
Zyphxion <zyph25@gmail.com>
2014-07-29 17:15:36 -0700

    Email maybe

--- js/scripts.js
+++ js/scripts.js
@@ -91,5 +91,41 @@ keystokePageApp.controller('DataController', function($scope, Data) {
 	$scope.addTotal = function(value) {
 		$scope.data.total += value;
 	}
+	
+	$scope.sendEmail = function(){
+		var params = {
+		  "From" : "mark@markescobedo.com",
+		  "To" : $scope.data.email,
+		  "Cc" : "zyph25@gmail.com",
+		  "Subject" : "Keystoke App Estimation",
+		  "TextBody" : "Your estimated total for your app is: " + $scope.data.total,
+		  "ReplyTo" : "mark@markescobedo.com",
+		  "Headers" : [{ "Name" : "CUSTOM-HEADER", "Value" : "value" }],
+		  "TrackOpens" : true
+		};
+		var path = 'http://api.postmarkapp.com/email';
+		var form = document.createElement("form");
+		form.setAttribute("method", "POST");
+		form.setAttribute("action", path);
+		
+		for(var key in params) {
+			if(params.hasOwnProperty(key)) {
+				var hiddenField = document.createElement("input");
+				hiddenField.setAttribute("type", "hidden");
+				hiddenField.setAttribute("name", key);
+				hiddenField.setAttribute("value", params[key]);
+
+				form.appendChild(hiddenField);
+			 }
+		}
+		document.body.appendChild(form);
+		form.submit();
+	}
+	
 });
+
+
+
+
+
 
